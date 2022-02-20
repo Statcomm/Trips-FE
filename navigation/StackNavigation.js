6364b2b07936343e08178f888c886ffc68735302
a1000rhan <1000rhan@gmail.com>
2022-02-20 10:44:50 +0300

    start up

---
+++ navigation/StackNavigation.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const StackNavigation = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default StackNavigation;
+
+const styles = StyleSheet.create({});
